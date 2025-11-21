2a022f5bc52d83293e8038bfd5becf2f01cb1bdf
Jakob <jwaldh@outlook.com>
2025-11-21 19:51:32 +0100

    Created .txt palette parser
    - Added a .txt palette parser to ease the process of adding palettes
    - processing functions now use buffers instead of Dynamic Image

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,10 @@
 mod cli;
 mod processor;
-mod palettes;
 
 use clap::Parser;
 use cli::Args;
-use cli::print_selection;
-use crate::cli::{validate_input, validate_output};
-use crate::palettes::{EVERFOREST, GRUVBOX};
-use crate::processor::process_image;
+use processor::*;
+use cli::*;
 
 fn main() {
     // ------------------------------------------ INPUT -------------------------------------------
@@ -20,11 +17,13 @@ fn main() {
     // ---------------------------------------- PROCESSING ----------------------------------------
     
     let img = image::open(&args.input).expect("failed to open input image");
+    let buf = img.into_rgba8();
+    let pal_path = format!("palettes/{}.txt", args.palette.as_str());
     println!(" Converting to:  \x1b[33;1m{}\x1b[0m", args.palette.as_str());
     let processed = match args.palette.as_str() {
-        "gray" => processor::process_gray(img),
-        "gruvbox" => process_image(img, &GRUVBOX),
-        "everforest" => process_image(img, &EVERFOREST),
+        "gray" => processor::process_gray(buf),
+        "gruvbox" => process_image(buf, &*get_palette(pal_path.as_str())),
+        //"everforest" => process_image(img, &EVERFOREST),
         _ => panic!("Unrecognized palette: {}", args.palette),
     };
     processed.save(&output).expect("failed to save image");
